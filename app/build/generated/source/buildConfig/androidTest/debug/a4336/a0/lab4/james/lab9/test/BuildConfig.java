8dba7c99a6477158653150d1ce51efd751beeb7b
JamesAlexander1 <jamesbrooksalexander@gmail.com>
2016-10-03 18:56:18 +1100

    moving to branch

---
+++ app/build/generated/source/buildConfig/androidTest/debug/a4336/a0/lab4/james/lab9/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package a4336.a0.lab4.james.lab9.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "a4336.a0.lab4.james.lab9.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
