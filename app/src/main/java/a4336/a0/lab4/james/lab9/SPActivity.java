129767ae961613bc503e8209c5166425862a32dd
JamesAlexander1 <jamesbrooksalexander@gmail.com>
2016-10-03 19:25:43 +1100

    SPactivity

--- app/src/main/java/a4336/a0/lab4/james/lab9/SPActivity.java
+++ app/src/main/java/a4336/a0/lab4/james/lab9/SPActivity.java
@@ -2,12 +2,24 @@ package a4336.a0.lab4.james.lab9;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SPActivity extends AppCompatActivity {
 
+    private String fileName = "Lab9File";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sp);
+
+        final Button SPBackBtn = (Button) findViewById(R.id.SPBackbutton);
+        SPBackBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
