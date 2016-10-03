f0842b65c87c58258acd13c40091c08bb171e9e7
JamesAlexander1 <jamesbrooksalexander@gmail.com>
2016-10-03 19:13:40 +1100

    new activiies

--- app/src/main/java/a4336/a0/lab4/james/lab9/MainActivity.java
+++ app/src/main/java/a4336/a0/lab4/james/lab9/MainActivity.java
@@ -1,7 +1,10 @@
 package a4336.a0.lab4.james.lab9;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        final Button statusButton = (Button) findViewById(R.id.SPbutton);
+
+        statusButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SPActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/a4336/a0/lab4/james/lab9/SPActivity.java
@@ -0,0 +1,13 @@
+package a4336.a0.lab4.james.lab9;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SPActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sp);
+    }
+}
